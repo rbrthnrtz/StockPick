229160d429a0ad16c4912ef8112d2e360d932820
rbrthnrtz <60026083+rbrthnrtz@users.noreply.github.com>
2023-12-16 18:34:45 -0500

    added comments detailing code function for reference

--- Public/script.js
+++ Public/script.js
@@ -1,17 +1,23 @@
+// Add event listener for the 'DOMContentLoaded' event
 document.addEventListener('DOMContentLoaded', () => {
+    // Get the stock form and result div elements from the DOM
     const stockForm = document.getElementById('stock-form');
     const resultDiv = document.getElementById('result');
 
+    // Add event listener for the 'submit' event on the stock form
     stockForm.addEventListener('submit', async (event) => {
-        event.preventDefault();
-        const symbol = document.getElementById('symbol').value;
+        event.preventDefault(); // Prevent the default form submit action
+        const symbol = document.getElementById('symbol').value; // Get the stock symbol from the form
 
         try {
+            // Make an asynchronous HTTP request to the server
             const response = await fetch(`/stock/${symbol}`);
-            const data = await response.json();
+            const data = await response.json(); // Parse the JSON response
 
+            // Check if data is received and contains the 'Global Quote' field
             if (data && data['Global Quote']) {
-                const quote = data['Global Quote'];
+                const quote = data['Global Quote']; // Extract the quote data
+                // Set the result div's innerHTML to show the stock data
                 resultDiv.innerHTML = `
                     <h2>${symbol} Stock Data</h2>
                     <p>Open: ${quote['02. open']}</p>
@@ -20,10 +26,12 @@ document.addEventListener('DOMContentLoaded', () => {
                     <p>Price: ${quote['05. price']}</p>
                 `;
             } else {
+                // Set the result div to show 'Stock data not found'
                 resultDiv.innerHTML = '<p>Stock data not found.</p>';
             }
         } catch (error) {
+            // Set the result div to show an error message if fetch fails
             resultDiv.innerHTML = '<p>Error fetching stock data.</p>';
         }
     });
-}); 
+});
--- app.js
+++ app.js
@@ -1,27 +1,30 @@
-const express = require('express');
-const axios = require('axios');
-const app = express();
-const PORT = process.env.PORT || 3000;
+const express = require('express'); // Import Express.js framework
+const axios = require('axios'); // Import Axios for HTTP requests
+const app = express(); // Initialize an Express application
+const PORT = process.env.PORT || 3000; // Set the port number, default to 3000 if not provided in environment
 
-require('dotenv').config();
+require('dotenv').config(); // Load environment variables from a .env file
 
-app.use(express.static('public'));
+app.use(express.static('public')); // Serve static files from the 'public' directory
 
+// Define a route for GET requests to '/stock/:symbol'
 app.get('/stock/:symbol', async (req, res) => {
-    const symbol = req.params.symbol;
+    const symbol = req.params.symbol; // Extract stock symbol from the request parameters
     const apiKey = process.env.ALPHA_VANTAGE_API_KEY; // Retrieve API key from environment variable
 
     try {
+        // Make an HTTP GET request to the Alpha Vantage API
         const response = await axios.get(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=${apiKey}`);
-        console.log(response.data);
-        const data = response.data;
-        res.json(data);
+        console.log(response.data); // Log the response data
+        const data = response.data; // Extract data from the response
+        res.json(data); // Send the data back to the client in JSON format
     } catch (error) {
-        console.error('Error fetching stock data:', error.message);
-        res.status(500).json({ error: 'Failed to fetch stock data' });
+        console.error('Error fetching stock data:', error.message); // Log any errors
+        res.status(500).json({ error: 'Failed to fetch stock data' }); // Send a 500 Internal Server Error response
     }
 });
 
+// Start the server and listen on the specified PORT
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`); // Log a message when the server starts
 });
